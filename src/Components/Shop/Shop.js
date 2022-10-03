4449f0160b6480e7d690cf60d1c98d6838d3f749
salsabeelnoor <salsabeelnoor@gmail.com>
2022-10-03 18:23:51 +0600

    done

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import { addToDb, getStoredCart } from '../Utilities/fakedb';
 import './Shop.css';
 
 const Shop = () => {
@@ -10,11 +12,40 @@ const Shop = () => {
         .then(res=> res.json())
         .then(data => setProducts(data));
     }, []);
-    const HandleAddToCart = (product) => {
-        console.log(product);
+
+    useEffect(() => {
+        //retrieve data from local storage
+        const storedCart = getStoredCart(); //returns an object
+        const savedCart = [];
+        for(const id in storedCart){ //loop through object
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products]);
+
+    const HandleAddToCart = (selectedProduct) => {
+        // console.log(product);
         // cart.push(product);
-        const newCart = [...cart, product]; //cart er product er shathe notun product add
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+        // const newCart = [...cart, selectedProduct]; //cart er product er shathe notun product add
         setCart(newCart);
+        addToDb(selectedProduct.id);
+        // console.log(newCart);
     }
     return (
         <div className='shop-container'>
@@ -28,8 +59,7 @@ const Shop = () => {
                }
             </div>
             <div className="cart-container">
-                <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
